a6580e3780c760dedf42d6e2ab57309ecb326fcb
jamilessnts <jamile.ssnts@gmail.com>
2016-10-17 18:11:11 -0300

    alguns ajustes finais

--- mysite/sistemaacademico/views.py
+++ mysite/sistemaacademico/views.py
@@ -97,16 +97,6 @@ def graficoteste(request):
     total_n3 = Avaliacoes.objects.all().aggregate(Sum('nota3'))
     return render(request, 'sistemaacademico/grafico.html', {'total_n1': total_n1, 'total_n2': total_n2, 'total_n3': total_n3})
 
-def some_view(request):
-    # Create the HttpResponse object with the appropriate CSV header.
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
-
-    writer = csv.writer(response)
-    writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
-    writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
-
-    return response
 
 def export_csv(request):
     response = HttpResponse(content_type='text/csv')
